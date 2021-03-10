19e0549aa1c13b29dec48478191091caa8720808
Elkana Orbach <elkana.orbach@au10tix.com>
2021-03-10 14:08:42 +0200

    check for nullable url

--- SecureMe/ViewController.swift
+++ SecureMe/ViewController.swift
@@ -20,15 +20,16 @@ class ViewController: UIViewController, WKNavigationDelegate {
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		webView.load(url.absoluteString) //"https://dev.10tix.me/CUu99zG6zne14FodO0Ut")
-
+		if url != nil {
+			webView.load(url.absoluteString) //"https://dev.10tix.me/CUu99zG6zne14FodO0Ut")
+		}
 	}
-
+	
 	func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
 		webView.reload()
 	}
 }
- 
+
 extension WKWebView {
 	func load(_ urlString: String) {
 		if let url = URL(string: urlString) {
