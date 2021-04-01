a7ef643ad00aae402cb746c3bc76dd0b0bcfc6f7
benny_d <benny.davidovitz@au10tix.com>
2021-04-01 14:51:16 +0300

    using WKWebView Configs to fix bugs

--- SecureMe/ViewController.swift
+++ SecureMe/ViewController.swift
@@ -10,17 +10,37 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
 
-	@IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var containerView: UIView!
+    weak var webView: WKWebView!
 	
 	var url: URL!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+        
+        let configs = WKWebViewConfiguration()
+        configs.setValue(true, forKey: "_allowUniversalAccessFromFileURLs")
+        configs.preferences.javaScriptCanOpenWindowsAutomatically = true
+        configs.preferences.javaScriptEnabled = true
+        
+        let webView = WKWebView(frame: view.bounds, configuration: configs)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.addSubview(webView)
+        
+        [
+            webView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
+        ].forEach { $0.isActive = true }
+        
+        self.webView = webView
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		if url != nil {
+        if let url = self.url {
 			webView.load(url.absoluteString) //"https://dev.10tix.me/CUu99zG6zne14FodO0Ut")
 		}
 	}
