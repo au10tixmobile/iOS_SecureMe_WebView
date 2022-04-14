b72689f13adde751b69c7704da36fb65cf2ca1c6
Serhii Kostrykin <serhii.kostrykin@au10tix.com>
2022-04-14 18:53:03 +0300

    Flow updated

--- SecureMe/ViewController.swift
+++ SecureMe/ViewController.swift
@@ -8,57 +8,87 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var containerView: UIView!
+    // MARK: - Properties
     weak var webView: WKWebView!
-	
     private var secureMeFinishedMessage: String { "webview" }
+
+    // MARK: - Outlets
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var startView: UIView!
+    @IBOutlet weak var textField: UITextField!
+
+    // MARK: - Actions
+    @IBAction func startButtonClicked(_ sender: UIButton) {
+        if let text = textField.text, let url = URL(string: text) {
+            load(url: url)
+            textField.resignFirstResponder()
+            startView.isHidden = true
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Please enter correct URL", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    }
     
+    // MARK: - Methods
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
         createWebView()
 	}
     
-    private func createWebView() {
+    func load(url: URL) {
+        webView.load(URLRequest(url: url))
+    }
+
+}
+
+// MARK: - Private API
+private extension ViewController {
+    
+    func createWebView() {
         let configs = WKWebViewConfiguration()
         configs.allowsInlineMediaPlayback = true
         configs.userContentController.add(self, name: secureMeFinishedMessage)
-        
         let webView = WKWebView(frame: view.bounds, configuration: configs)
         webView.translatesAutoresizingMaskIntoConstraints = false
-        
         containerView.addSubview(webView)
-        
         [
             webView.topAnchor.constraint(equalTo: containerView.topAnchor),
             webView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
             webView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
         ].forEach { $0.isActive = true }
-        
         self.webView = webView
     }
-    
-    func load(url: URL) {
-        webView.load(URLRequest(url: url))
-    }
-	
-	func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
-		webView.reload()
-	}
 }
 
+// MARK: - WKNavigationDelegate
+extension ViewController: WKNavigationDelegate {
+    func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+        webView.reload()
+    }
+}
+
+// MARK: - WKScriptMessageHandler
 extension ViewController: WKScriptMessageHandler {
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        
         if message.name == self.secureMeFinishedMessage {
             //handle the finish event
-            let obj = JSEventObj(dict: message.body as? [String:Any] ?? [:])
+            let payload = JSEventObj(body: message.body)
+            switch payload?.event {
+            case .success, .error:
+                let alert = UIAlertController(title: payload?.event == .success ? "Success" : "Error", message: payload?.payload?.value, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Restart", style: .default) { [weak self] _ in
+                    self?.webView.evaluateJavaScript("document.body.remove()")
+                    self?.textField.text = nil
+                    self?.startView.isHidden = false
+                })
+                present(alert, animated: true)
+            default: break
+            }
         }
-        
     }
-    
 }
