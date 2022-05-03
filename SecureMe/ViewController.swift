6d2b377666c03be29d3de2b7bcb98841e292a3fb
Serhii Kostrykin <serhii.kostrykin@au10tix.com>
2022-05-03 14:05:31 +0100

    Permissions added

--- SecureMe/ViewController.swift
+++ SecureMe/ViewController.swift
@@ -22,9 +22,9 @@ class ViewController: UIViewController {
     // MARK: - Actions
     @IBAction func startButtonClicked(_ sender: UIButton) {
         if let text = textField.text, let url = URL(string: text) {
-            load(url: url)
             textField.resignFirstResponder()
             startView.isHidden = true
+            load(url: url)
         } else {
             let alert = UIAlertController(title: "Error", message: "Please enter correct URL", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default))
@@ -35,13 +35,13 @@ class ViewController: UIViewController {
     // MARK: - Methods
 	override func viewDidLoad() {
 		super.viewDidLoad()
+        textField.text = ""
         createWebView()
 	}
     
     func load(url: URL) {
         webView.load(URLRequest(url: url))
     }
-
 }
 
 // MARK: - Private API
@@ -83,6 +83,7 @@ extension ViewController: WKScriptMessageHandler {
                 let alert = UIAlertController(title: payload?.event == .success ? "Success" : "Error", message: payload?.payload?.value, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "Restart", style: .default) { [weak self] _ in
                     self?.webView.evaluateJavaScript("document.body.remove()")
+                    self?.webView.load(URLRequest(url: URL(string: "about:blank")!))
                     self?.textField.text = nil
                     self?.startView.isHidden = false
                 })
